fd11db2b20adb6992251632c9350f339e2774f9f
Maggie Eminizer <margaret.eminizer@gmail.com>
2023-09-20 15:10:14 -0400

    only the version test should fail now

--- test/test_scripts/test_version.py
+++ test/test_scripts/test_version.py
@@ -29,9 +29,6 @@ class TestVersion(unittest.TestCase):
     def test_version_incremented(self):
         """Make sure the current version from PyPI is less than the version from the
         package as it is right now"""
-        d = {"a": 1, "b": 2}
-        for key in d.keys():
-            print(key)
         pypi_version = parse(self.get_latest_pypi_version("openmsitoolbox"))
         try:
             current_version = parse(openmsitoolbox.__version__)
