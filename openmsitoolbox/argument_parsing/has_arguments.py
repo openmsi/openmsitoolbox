cc200133c32c3b2ff48adb5d819b76b44b5b4430
Maggie Eminizer <margaret.eminizer@gmail.com>
2024-01-09 13:17:08 -0500

    add a new method to HasArguments

--- openmsitoolbox/argument_parsing/has_arguments.py
+++ openmsitoolbox/argument_parsing/has_arguments.py
@@ -27,3 +27,18 @@ class HasArguments(ABC):
         :rtype: dict
         """
         return [], {}
+    
+    @classmethod
+    @abstractmethod
+    def get_init_args_kwargs(cls, parsed_args) -> Tuple[List[str], Dict[str, Any]]:
+        """Get the list of init arguments and the dictionary of init keyword arguments
+        for this class given a namespace of, for example, parsed arguments.
+        
+        :param parsed_args: A namespace containing entries needed to determine the init
+            args and kwargs for this class
+        :type parsed_args: argparse.Namespace
+
+        :return: A list of init args
+        :return: A dictionary of init kwargs
+        """
+        return [], {}
