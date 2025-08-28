c1b56efc519a9c098d555a61cc10fa0579925751
arachid1 <arachid1@jhu.edu>
2025-08-28 14:47:29 -0400

    using black formatter

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import pathlib
 import setuptools
 
 VERSION = None
-VERSION_PATH = pathlib.Path(__file__).parent/"openmsitoolbox"/"version.py"
-with open(VERSION_PATH,"r") as version_file:
+VERSION_PATH = pathlib.Path(__file__).parent / "openmsitoolbox" / "version.py"
+with open(VERSION_PATH, "r") as version_file:
     for line in version_file.readlines():
         if line.startswith("__version__"):
             VERSION = line.strip().split("=")[-1].strip().strip('"')
@@ -19,25 +19,25 @@ with open("README.md", "r") as readme:
         LONG_DESCRIPTION += line
 
 setupkwargs = {
-    "name":"openmsitoolbox",
-    "packages":setuptools.find_packages(include=["openmsitoolbox*"]),
-    "include_package_data":True,
-    "version":VERSION,
-    "description":(
+    "name": "openmsitoolbox",
+    "packages": setuptools.find_packages(include=["openmsitoolbox*"]),
+    "include_package_data": True,
+    "version": VERSION,
+    "description": (
         "Python utilities for OpenMSI projects "
         "Developed for Open MSI (NSF DMREF award #1921959)"
     ),
-    "long_description":LONG_DESCRIPTION,
-    "long_description_content_type":"text/markdown",
-    "author":"OpenMSI Dev Team",
-    "author_email":"openmsistream@gmail.com",
-    "url":"https://github.com/openmsi/openmsitoolbox",
-    "download_url":f"https://github.com/openmsi/openmsitoolbox/archive/refs/tags/v{VERSION}.tar.gz",
-    "license":"GNU GPLv3",
-    "license_files":["LICENSE"],
-    "python_requires":">=3.7",
-    "install_requires":[],
-    "extras_require":{
+    "long_description": LONG_DESCRIPTION,
+    "long_description_content_type": "text/markdown",
+    "author": "OpenMSI Dev Team",
+    "author_email": "openmsistream@gmail.com",
+    "url": "https://github.com/openmsi/openmsitoolbox",
+    "download_url": f"https://github.com/openmsi/openmsitoolbox/archive/refs/tags/v{VERSION}.tar.gz",
+    "license": "GNU GPLv3",
+    "license_files": ["LICENSE"],
+    "python_requires": ">=3.7",
+    "install_requires": [],
+    "extras_require": {
         "test": [
             "black",
             "packaging",
@@ -49,11 +49,11 @@ setupkwargs = {
             "twine",
         ],
     },
-    "keywords":[
+    "keywords": [
         "materials",
         "data_science",
     ],
-    "classifiers":[
+    "classifiers": [
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
